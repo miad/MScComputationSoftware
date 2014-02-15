899799809b5c658b354f6693b1a9f06a97cf4f17
Rikard Lundmark <rikard.lundmark@yahoo.se>
2014-02-15 17:40:54 +0100

    Some minor changes, mostly bugfixes.

--- RunWhenAvailable.sh
+++ RunWhenAvailable.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Runs a program when more memory than 41 GB becomes available.
-while [[ "`grep MemFree /proc/meminfo | awk '{print $2}'`" -lt "42991616" ]]
+while [[ "`ps -u riklund | grep Compute | wc`" -gt "2" ]]
 do
 	sleep 60
 done
